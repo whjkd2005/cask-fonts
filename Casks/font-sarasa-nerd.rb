877bd05eb6f952f4a92d1d3019960bfc8c4c4f4f
whjkd2005 <59304699+whjkd2005@users.noreply.github.com>
2021-05-16 17:20:56 +0800

    创建 font-sarasa-nerd.rb
    update

---
+++ Casks/font-sarasa-nerd.rb
@@ -0,0 +1,18 @@
+cask "font-sarasa-nerd" do
+  version "2.0.1"
+
+  url "https://github.com/laishulu/Sarasa-Mono-SC-Nerd/archive/v#{version}.zip"
+  name "Sarasa Mono SC Nerd font"
+  homepage "https://github.com/laishulu/Sarasa-Mono-SC-Nerd"
+
+  font "Sarasa-Mono-SC-Nerd-#{version}/sarasa-mono-sc-nerd-bold.ttf"
+  font "Sarasa-Mono-SC-Nerd-#{version}/sarasa-mono-sc-nerd-bolditalic.ttf"
+  font "Sarasa-Mono-SC-Nerd-#{version}/sarasa-mono-sc-nerd-extralight.ttf"
+  font "Sarasa-Mono-SC-Nerd-#{version}/sarasa-mono-sc-nerd-extralightitalic.ttf"
+  font "Sarasa-Mono-SC-Nerd-#{version}/sarasa-mono-sc-nerd-italic.ttf"
+  font "Sarasa-Mono-SC-Nerd-#{version}/sarasa-mono-sc-nerd-light.ttf"
+  font "Sarasa-Mono-SC-Nerd-#{version}/sarasa-mono-sc-nerd-lightitalic.ttf"
+  font "Sarasa-Mono-SC-Nerd-#{version}/sarasa-mono-sc-nerd-regular.ttf"
+  font "Sarasa-Mono-SC-Nerd-#{version}/sarasa-mono-sc-nerd-semibold.ttf"
+  font "Sarasa-Mono-SC-Nerd-#{version}/sarasa-mono-sc-nerd-semibolditalic.ttf"
+end
