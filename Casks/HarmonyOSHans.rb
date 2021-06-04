77c45d88cb0853f7b9d96cb670b8eb7288eee409
whjkd2005 <59304699+whjkd2005@users.noreply.github.com>
2021-06-04 19:52:54 +0800

    Create HarmonyOSHans.rb

---
+++ Casks/HarmonyOSHans.rb
@@ -0,0 +1,17 @@
+cask "HarmonyOSHans" do
+  version "1.0"
+
+  url "https://github.com/whjkd2005/HarmonyOSHans/archive/v#{version}.zip"
+  name "HarmonyOSHans font"
+  homepage "https://github.com/whjkd2005/HarmonyOSHans"
+
+  font "HarmonyOSHans-#{version}/HarmonyOSHans Black.ttf"
+  font "HarmonyOSHans-#{version}/HarmonyOSHans Bold.ttf"
+  font "HarmonyOSHans-#{version}/HarmonyOSHans Heavy.ttf"
+  font "HarmonyOSHans-#{version}/HarmonyOSHans Light.ttf"
+  font "HarmonyOSHans-#{version}/HarmonyOSHans Medium.ttf"
+  font "HarmonyOSHans-#{version}/HarmonyOSHans Regular.ttf"
+  font "HarmonyOSHans-#{version}/HarmonyOSHans SemiBold.ttf"
+  font "HarmonyOSHans-#{version}/HarmonyOSHans Thin.ttf"
+  font "HarmonyOSHans-#{version}/HarmonyOSHans UltraLight.ttf"
+end
