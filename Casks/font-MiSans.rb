6344f3495cf57db655e95c134b9df587f672a949
whjkd2005 <59304699+whjkd2005@users.noreply.github.com>
2022-01-04 16:29:18 +0800

    Create font-MiSans.rb

---
+++ Casks/font-MiSans.rb
@@ -0,0 +1,18 @@
+cask "font-MiSans" do
+  version "2.0"
+
+  url "https://github.com/whjkd2005/MiSans-Fonts/archive/refs/tags/#{version}.zip"
+  name "MiSans font"
+  homepage "https://github.com/whjkd2005/MiSans-Fonts"
+
+  font "MiSans-Fonts-#{version}/MiSans-Bold.ttf"
+  font "MiSans-Fonts-#{version}/MiSans-Demibold.ttf"
+  font "MiSans-Fonts-#{version}/MiSans-ExtraLight.ttf"
+  font "MiSans-Fonts-#{version}/MiSans-Heavy.ttf"
+  font "MiSans-Fonts-#{version}/MiSans-Light.ttf"
+  font "MiSans-Fonts-#{version}/MiSans-Medium.ttf"
+  font "MiSans-Fonts-#{version}/MiSans-Normal.ttf"
+  font "MiSans-Fonts-#{version}/MiSans-Regular.ttf"
+  font "MiSans-Fonts-#{version}/MiSans-Semibold.ttf"
+  font "MiSans-Fonts-#{version}/MiSans-Thin.ttf"
+end
