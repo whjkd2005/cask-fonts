2640296d363fc3d23fd3231e6821c83fec3fe653
whjkd2005 <59304699+whjkd2005@users.noreply.github.com>
2022-12-07 08:25:12 +0800

    Create font-HONORSansCN.rb

---
+++ Casks/font-HONORSansCN.rb
@@ -0,0 +1,17 @@
+cask "font-HONORSansCN" do
+  version "1.0"
+
+  url "https://github.com/whjkd2005/HONORSansCN/archive/v#{version}.zip"
+  name "harmonyoshans"
+  homepage "https://github.com/whjkd2005/HONORSansCN"
+
+  font "HONORSansCN-#{version}/HONORSansCN-Bold.ttf"
+  font "HONORSansCN-#{version}/HONORSansCN-DemiBold.ttf"
+  font "HONORSansCN-#{version}/HONORSansCN-ExtraBold.ttf"
+  font "HONORSansCN-#{version}/HONORSansCN-ExtraLight.ttf"
+  font "HONORSansCN-#{version}/HONORSansCN-Heavy.ttf"
+  font "HONORSansCN-#{version}/HONORSansCN-Light.ttf"
+  font "HONORSansCN-#{version}/HONORSansCN-Medium.ttf"
+  font "HONORSansCN-#{version}/HONORSansCN-Regular.ttf"
+  font "HONORSansCN-#{version}/HONORSansCN-Thin.ttf"
+end
