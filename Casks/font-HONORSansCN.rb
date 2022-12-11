23e3d91338855d7bbdb8839c1cc5c16d15b08cc9
whjkd2005 <59304699+whjkd2005@users.noreply.github.com>
2022-12-11 16:15:44 +0800

    Update font-HONORSansCN.rb

--- Casks/font-HONORSansCN.rb
+++ Casks/font-HONORSansCN.rb
@@ -1,7 +1,7 @@
 cask "font-HONORSansCN" do
   version "1.0"
 
-  url "https://github.com/whjkd2005/HONORSansCN-Fonts/archive/v#{version}.zip"
+  url "https://github.com/whjkd2005/HONORSansCN-Fonts/archive/refs/tags/{version}.zip"
   name "HONORSansCN"
   homepage "https://github.com/whjkd2005/HONORSansCN-Fonts"
 
